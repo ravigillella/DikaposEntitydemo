0b6771871fdbd98b6db09ff3c4b3ba10db23f556
Sandeep <sanday1994@gmail.com>
2017-03-28 12:05:36 +0530

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationOwinDemo.Startup))]
+namespace WebApplicationOwinDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
